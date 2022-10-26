026fe3cb16026e9e52da9a517e886b671ffdd570
bethanyallen42 <bethanyallen42@gmail.com>
2022-10-25 19:37:23 -0600

    basic components and routes created

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import { NavLink, Routes, Route } from "react-router-dom";
+import { Home, Portfolio, Blog } from "./";
+
 const App = () => {
-  return <div>Hey there</div>;
+  return (
+    <main>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+
+        <NavLink to="/portfolio">Portfolio</NavLink>
+
+        <NavLink to="/Blog">Blog</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
 export { default as App } from "./App";
+export { default as Home } from "./Home";
+export { default as Portfolio } from "./Portfolio";
+export { default as Blog } from "./Blog";
