68a3927d0c6ad5a1a2163ae907f7890fad974585
bethanyallen42 <bethanyallen42@gmail.com>
2022-10-26 22:47:24 -0600

    navbar created and styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,51 @@
 import React from "react";
 import { NavLink, Routes, Route } from "react-router-dom";
 import { Home, Portfolio, Blog } from "./";
+import "../style/App.css";
 
 const App = () => {
   return (
-    <main>
-      <nav>
-        <NavLink to="/">Home</NavLink>
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            Bethany Allen
+          </a>
+          <button
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#nav"
+            aria-controls="nav"
+            aria-label="Expand Navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="nav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="/" className="nav-link active" aria-current="page">
+                  Home
+                </a>
+              </li>
 
-        <NavLink to="/portfolio">Portfolio</NavLink>
+              <li className="nav-item">
+                <a
+                  href="/portfolio"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Portfolio
+                </a>
+              </li>
 
-        <NavLink to="/Blog">Blog</NavLink>
+              <li className="nav-item">
+                <a href="/blog" className="nav-link active" aria-current="page">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
       <Routes>
@@ -18,7 +53,7 @@ const App = () => {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
-    </main>
+    </>
   );
 };
 
