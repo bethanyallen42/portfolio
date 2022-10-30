561bb27f05dfc5302c5e2aed9cec55bdefa7e9c3
bethanyallen42 <bethanyallen42@gmail.com>
2022-10-29 20:51:08 -0600

    styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "../style/Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="top-triangle"></div>
+      <div className="this">Home</div>
+      <div className="bottom-triangle"></div>
+    </>
+  );
 };
 
 export default Home;
